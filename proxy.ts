0c577fdac02c3ef54ba74c3f324536350ed65306
Jan Van Riel <=>
2022-12-05 09:58:00 +0100

    added trace header

--- proxy.ts
+++ proxy.ts
@@ -19,6 +19,7 @@ const forward = async (url:string) => {
     const headers = new Headers()
     headers.set('x-twintag-url', url)
     headers.set('x-twintag-method', 'GET')
+    headers.set('x-twintag-denodeploy-trace', `${Math.floor(Date.now())}`)
     const rsp = await fetch('https://worker-proxy.sosvertigo.workers.dev', {
       method: 'GET',
       headers: headers,
